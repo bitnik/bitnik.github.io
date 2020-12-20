49acd595c78d64bef538d96d9215b93e20681452
Kenan Erdogan <kenanerdogan@gmail.com>
2020-12-20 14:01:08 +0100

    update

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -30,11 +30,10 @@
     {
       title: '<a href="https://www.gesis.org/en/home">GESIS – Leibniz Institute for the Social Sciences</a>, Cologne, Germany',
       date: '10.2017 - 12.2020',
+      position: 'Lead Software Developer',
       badges: ['python', 'JupyterHub', 'BinderHub', 'repo2docker', 'Docker', 'Kubernetes', 'Helm', 'NFS Provisioner', 'GitLab CI/CD', 'Prometheus', 'Grafana', 'Elastic Stack'],
       content: `
-Lead Software Developer
-
-[GESIS Notebooks](https://notebooks.gesis.org/about/) is a free and hosted service for running Jupyter Notebooks requiring no installation, 
+**Project:** [GESIS Notebooks](https://notebooks.gesis.org/about/) is a free and hosted service for running Jupyter Notebooks requiring no installation, 
 which enable you to get started quickly with your research ideas, prototyping your data science projects, 
 or learning to program languages such as R, Python or Julia .
 
@@ -44,12 +43,12 @@ or learning to program languages such as R, Python or Julia .
 - Setup and maintenance of the servers and the kubernetes cluster
 - Deployment and maintenance of [a Persistent BinderHub](https://notebooks.gesis.org/), [a public BinderHub](https://notebooks.gesis.org/binder/) 
 (GitHub repo: [gesiscss/orc](https://github.com/gesiscss/orc))
-- Automation of all aspects including image building, chart and configuration updates, and deployments
+- Automation of all aspects including image building, chart and configuration updates and deployments
 - Initial implementation of the [GESIS Login](https://login.gesis.org/) system
 - Cooperation with open source projects (Jupyter projects) and contributing
 - Jupytercon 2020 Talk: ["A Persistent BinderHub: Democratizing Access to Computational Resources in the Social Sciences"](https://www.youtube.com/watch?v=dwidVhPtQT4)
-- Workshop: ["Introduction to Python"](https://hf-graduiertenschule.uni-koeln.de/index.php?id=32102&L=1)
-- Publication: ["iLCM - A Virtual Research Infrastructure for Large-Scale Qualitative Data."](https://arxiv.org/abs/1805.11404)
+- Tutoring in ["Introduction to Python"](https://hf-graduiertenschule.uni-koeln.de/index.php?id=32102&L=1) workshop
+- Publication of ["iLCM - A Virtual Research Infrastructure for Large-Scale Qualitative Data."](https://arxiv.org/abs/1805.11404)
 
 Reference: [Dr. Arnim Bleier](https://www.gesis.org/en/institute/staff/person/arnim.bleier)
 `
@@ -57,11 +56,10 @@ Reference: [Dr. Arnim Bleier](https://www.gesis.org/en/institute/staff/person/ar
     {
       title: '<a href="https://www.gesis.org/en/home">GESIS – Leibniz Institute for the Social Sciences</a>, Cologne, Germany',
       date: '07.2016 - 09.2017',
+      position: 'Lead Software Developer',
       badges: ['python', 'Django', 'Gunicorn', 'REST API', 'Swagger', 'Celery', 'RabbitMQ', 'WikiPedia'],
       content: `
-Lead Software Developer
-
-[WikiWho](https://github.com/wikiwho/WikiWho) is an algorithm to parse 
+**Project:** [WikiWho](https://github.com/wikiwho/WikiWho) is an algorithm to parse 
 the complete set of all historical revisions of a revisioned Wiki document 
 in order to find out who wrote and/or removed and/or reinserted which exact text at token (~word) level at what revision. 
 This means that for every token, its individual add/removal/reintroduction history becomes available. 
@@ -69,10 +67,10 @@ This means that for every token, its individual add/removal/reintroduction histo
 **Responsibilities:**
 
 - Speed and accuracy improvement of the existing core WikiWho algorithm
-- Realisation of [the website, the REST API, and a web-based API interface](https://www.wikiwho.net/)
+- Realisation of [the website, the REST API and a web-based API interface](https://www.wikiwho.net/)
 - Realisation of a listener for Wikipedia events, connected to a task queue
 - Update and improvement of [the WhoColor server and user tools](https://github.com/wikiwho/WhoColor)
-- Data analysis: authorship, persistence, conflict, editor interactions
+- Data analyses of authorship, persistence, conflict and editor interactions
 - Generation of a [data set](https://doi.org/10.5281/zenodo.834557) for the entire English Wikipedia: 
 ["TokTrack: A Complete Token Provenance and Change Tracking Dataset for the English Wikipedia"](https://arxiv.org/abs/1703.08244)
 
@@ -82,42 +80,38 @@ Reference: [Dr. Fabian Flöck](https://www.gesis.org/en/institute/staff/person/f
     {
       title: '<a href="https://www.mir.de/">MIR MEDIA</a>, Cologne, Germany',
       date: '07.2015 - 06.2016',
-      badges: ['Backend Development', 'python', 'Django'],
-      content: `
-Software Developer
-      `
+      position: 'Software Developer',
+      badges: ['python', 'Django'],
+      content: 'Backend development of websites for customers such as Elbphilarmonie and Lucerne Festival'
     },
+    // comment below to have PDF version 2 pages 
     {
       title: 'Induux GmbH, Stuttgart, Germany',
       date: '11.2014 - 03.2015',
-      badges: ['Backend Development', 'python', 'Django'],
-      content: `
-Software Developer
-`
+      position: 'Software Developer',
+      badges: ['python', 'Django'],
+      content: 'Backend development'
     },
     {
       title: 'Elektrobit Automotive GmbH, Böblingen, Germany',
       date: '11.2013 - 05.2014',
+      position: 'Student Employee',
       badges: ['python', 'Django', 'Selenium', 'REST API'],
-      content: `
-Student Employee
-`
+      content: ''
     },
     {
       title: 'Elektrobit Automotive GmbH, Böblingen, Germany',
       date: '04.2013 - 09.2013',
+      position: 'Intern',
       badges: ['python', 'software versioning', 'Unit testing', 'GUI development', 'CAN Bus', 'CANoe'],
-      content: `
-Intern
-`
+      content: ''
     },
     {
       title: 'PTV AG, Stuttgart, Germany',
       date: '11.2012 - 03.2013',
+      position: 'Student Employee',
       badges: ['C++', 'Visualization', 'The Visualization ToolKit'],
-      content: `
-Student Employee
-`
+      content: ''
     },
   ],
   education: [
@@ -127,8 +121,8 @@ Student Employee
       content: `
 M.Sc. in Information Technology  
 
-##### Master Thesis, Industrial Automation Systems Institute, 12.2013 - 05.2014
-Realization of incremental learning for automated identification using the example of products recognition
+##### Master Thesis at Industrial Automation Systems Institute
+Topic: Realization of incremental learning for automated identification using the example of products recognition
 
 - Incremental Learning, Machine Learning
 - Classification, Support Vector Machines (SVM)
@@ -144,17 +138,17 @@ Realization of incremental learning for automated identification using the examp
       content: `
 B.Sc. in Electronics Engineering (graduation in honor list)  
 
-##### Bachelor Thesis, Embedded System Design Laboratory, 02.2011 - 05.2011
-Design and implementation of a server on graphic processor for secure RFID systems
+##### Bachelor Thesis at Embedded System Design Laboratory
+Topic: Design and implementation of a server on graphic processor for secure RFID systems
 
 - Parallel programming
 - CUDA, GPU programming
 - RFID protocol servers
 
-##### Activities
+##### Extracurricular Activities
 
 - 09.2007 - 06.2011 : Member of the ITU Control and Automation Student Society
-- 09.2008 - 06.2009 : Administrative Committee Member of the ITU Robotic Olympics
+- 09.2008 - 06.2009 : Committee Member of the ITU Robotic Olympics
 - 09.2008 - 01.2009 : Member of the ITU Mountaineering Club
 `
     },
@@ -165,14 +159,16 @@ Design and implementation of a server on graphic processor for secure RFID syste
 Exchange Semester
 `
     },
-    {
-      title: 'Adnan Menderes Anatolian High School, Istanbul, Turkey',
-      date: '09.2002 - 06.2006',
-      content: ''
-    }
+    // {
+    //   title: 'Adnan Menderes Anatolian High School, Istanbul, Turkey',
+    //   date: '09.2002 - 06.2006',
+    //   content: ''
+    // }
   ],
   skills: [
     ['Python', '8/10'],
+    ['JupyterHub', '++++'],
+    ['BinderHub', '++++'],
     //['git', '++++'],
     ['Django', '++++'],
     //['Flask', '++++'],
@@ -194,7 +190,7 @@ Exchange Semester
   ],
   languages: [
     ['Turkish', 'native'],
-    ['English', ''],
+    ['English', 'fluent'],
     ['German', 'B2'],
   ],
   interests: [
@@ -204,6 +200,8 @@ Exchange Semester
     'Pilates',
     'Reading',
     'Meditation',
-    //'Sewing'
+    'Sewing',
+    'Wood work',
+    // 'Bicycling'
   ]
 };
